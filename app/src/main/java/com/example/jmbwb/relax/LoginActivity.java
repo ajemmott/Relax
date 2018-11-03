0b973708605cbb49dc8ffecf9be49f25dc7dbf2f
Arturo Jemmott <jmbw.box@gmail.com>
2018-11-03 15:29:58 -0500

    fix para local.properties

--- app/src/main/java/com/example/jmbwb/relax/LoginActivity.java
+++ app/src/main/java/com/example/jmbwb/relax/LoginActivity.java
@@ -3,10 +3,15 @@ package com.example.jmbwb.relax;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
