fe9dfb7e340f8283708c8f6986284dd75f519567
IngridtRodriguez <ingridt.23@hotmail.com>
2018-11-24 13:24:53 -0500

    SQLite para el regustro del usuario

--- app/src/main/java/com/example/jmbwb/relax/MainActivity.java
+++ app/src/main/java/com/example/jmbwb/relax/MainActivity.java
@@ -5,15 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button button_login;
+    TextView tv_registrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button_login = findViewById(R.id.button_begin);
+        tv_registrar = findViewById(R.id.tv_registrar);
 
         button_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +27,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        tv_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
